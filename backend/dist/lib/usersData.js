7a904299932a23f031be3ccddea56916e2c8af4c
Lourence Ruhwaya <162551120+Anotida28@users.noreply.github.com>
2026-04-14 16:30:47 +0200

    Remove deprecated stock location balance, user data, authentication middleware, and routing files to streamline backend structure and improve maintainability.

--- backend/dist/lib/usersData.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsersData = exports.createUserData = void 0;
-var authData_1 = require("./authData");
-Object.defineProperty(exports, "createUserData", { enumerable: true, get: function () { return authData_1.createUserData; } });
-Object.defineProperty(exports, "getUsersData", { enumerable: true, get: function () { return authData_1.listUsersData; } });
--- backend/dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.get("/", (0, authMiddleware_1.requireRole)("ADMIN", "SUPER_ADMIN"), userController_1.getUsers);
-router.post("/", (0, authMiddleware_1.requireRole)("ADMIN", "SUPER_ADMIN"), userController_1.createUser);
-exports.default = router;
